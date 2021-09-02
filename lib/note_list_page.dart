c1387422b902b36313107ab332574960b4d8f779
jordydave <jordydave1@gmail.com>
2021-09-02 10:05:15 +0700

    second commit

---
+++ lib/note_list_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sqflite_study/db_provider.dart';
+import 'package:sqflite_study/note_add_update_page.dart';
+
+class NoteListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notes'),
+      ),
+      body: Consumer<DbProvider>(
+        builder: (context, provider, child) {
+          final notes = provider.notes;
+
+          return ListView.builder(
+            itemCount: notes.length,
+            itemBuilder: (context, index) {
+              final note = notes[index];
+              return Dismissible(
+                key: Key(note.id.toString()),
+                background: Container(color: Colors.red),
+                onDismissed: (direction) {
+                  provider.deleteNote(note.id!);
+                },
+                child: Card(
+                  child: ListTile(
+                    title: Text(note.title),
+                    subtitle: Text(note.description),
+                    onTap: () async {
+                      final selectedNote = await provider.getNoteById(note.id!);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return NoteAddUpdatePage(selectedNote);
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => NoteAddUpdatePage()));
+        },
+      ),
+    );
+  }
+}
